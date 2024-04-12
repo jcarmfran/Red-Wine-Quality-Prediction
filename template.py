78f4921cb5f865b4dd6f04d28a84581d6322f09b
Francisco Carmona <jcarmfran@gmail.com>
2024-04-12 13:36:19 -0500

    Reinitializing project folders architecture

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = "mlProject"
+project_name = "wine"
 
 list_of_files = [
     f"src/{project_name}/__init__.py",
